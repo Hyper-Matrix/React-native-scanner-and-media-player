1446cd78abe0d7d2fee464a295491802903e6aac
ABHISHEK KUMAR <abhishekkumar11122@gmail.com>
2020-07-01 18:00:41 +0530

    changed to modal- native paper

---
+++ App.js
@@ -0,0 +1,205 @@
+
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,Alert,ScrollView} from 'react-native';
+import { TextInput,Button } from 'react-native-paper';
+import { createStackNavigator, createAppContainer} from "react-navigation";
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
+ function App(props) {
+  return (
+    <View style={styles.container}>
+      <Button  mode="contained" 
+    onPress={()=>props.navigation.navigate('Scanner') }
+color="#00BCD4"
+labelStyle={{color:"white",letterSpacing:3}}
+ style ={{margin:5, height:50,padding:8,width:360,borderRadius:30,opacity:0.7,borderColor
+ :"black",borderWidth:3,padding:5,marginTop:22} }>
+    SCANNER
+  </Button>
+  <Button  mode="contained" 
+   
+color="#00BCD4"
+labelStyle={{color:"white",letterSpacing:3}}
+ style ={{margin:5, height:50,padding:8,width:360,borderRadius:30,opacity:0.7,borderColor
+ :"black",borderWidth:3,padding:5,marginTop:22} }>
+    PLAY VIDEO/AUDIO
+  </Button>
+      
+    </View>
+  );
+}
+function Scanner(props)
+{const[hasCameraPermission,sethasCameraPermission ]=useState(null)
+  const[isScanned,setisScanned ]=useState(false)
+  
+  
+  async function componentDidMount(){
+    // ask for camera permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    console.log(status);
+    if( status === "granted"){
+      
+      sethasCameraPermission(true)
+    }
+  else{
+    sethasCameraPermission(false)
+  }
+    
+    console.log(hasCameraPermission)
+  }
+  componentDidMount()
+  handleBarCodeScanned = ({ type, data }) => {
+    // Do something here
+    props.navigation.navigate('LoadingPage', {
+      data: data 
+    });
+}
+//const { hasCameraPermission, isScanned } = useState;
+
+if(hasCameraPermission === null){
+  console.log("Requesting permission");
+  return (
+    <Text style={{color:'white',fontSize:30,marginTop:10,alignSelf:'center',fontWeight:'bold',textShadowColor:'black',textShadowOffset: {width: -1, height: 1},
+textShadowRadius: 15,letterSpacing:2}}>REQUESTING PERMISSION</Text>
+ 
+  );
+}
+if(hasCameraPermission === false){
+  
+  return ( 
+    
+     Alert.alert("Please grant Camera permission")
+    
+  )
+}
+if(hasCameraPermission === true && !isScanned && props.navigation.isFocused() ){
+  return <View style = {{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  
+  }}>
+   <ScrollView>
+    <Text style={{color:'white',fontSize:30,marginTop:10,alignSelf:'center',fontWeight:'bold',textShadowColor:'black',textShadowOffset: {width: -1, height: 1},
+textShadowRadius: 15,letterSpacing:2}}>Scan code inside window</Text>
+<View style={{borderColor:'white',
+       borderRadius:10,
+       borderWidth:5,
+       backgroundColor:'rgba(0,0,0,0.6)',
+       flex: 1,
+       flexDirection: 'column',
+       justifyContent: 'center',
+       alignItems: 'center',}
+       }>
+    <BarCodeScanner
+      onBarCodeScanned = { isScanned ? undefined : handleBarCodeScanned }
+      style = {{
+        height:  600,
+        width: 350
+      }}
+    >
+    </BarCodeScanner>
+    </View>
+    </ScrollView>
+  </View>
+ 
+  
+}
+else{
+  return (
+    <Text>LOADING</Text>
+  );
+}
+}
+
+
+function Video(){
+  return(
+    <View style={styles.container}>
+   <Text>Hello video</Text>
+  </View>
+  )
+}
+function Decode(props){
+  return(
+    <View style={styles.container}>
+   <Text>{props.navigation.getParam("data", "NO-QR")}</Text>
+  </View>
+  )
+}
+class LoadingPage extends React.Component {
+  componentDidMount(){const data =this.props.navigation.getParam("data", "NO-QR")
+       // Start counting when the page is loaded
+       this.timeoutHandle = setTimeout(()=>{
+           this.props.navigation.navigate('Decode',{
+             data
+           })
+       }, 5000);
+       
+  }
+
+  componentWillUnmount(){
+       clearTimeout(this.timeoutHandle); 
+  }
+
+  render() {
+    
+    console.log(this.props.navigation.getParam("data", "NO-QR"))
+  return (
+      <Text>Loading</Text>
+     
+
+  );
+}
+
+
+}
+const Appnav = createStackNavigator(
+  {
+    App:App,
+    Scanner:Scanner,
+    Video:Video,
+    Decode:Decode,
+    LoadingPage:LoadingPage
+  },{
+    headerMode:'screen',
+    initialRouteName:'App',
+    title:"Home",
+    
+  },
+  
+)
+export default createAppContainer(Appnav);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image:{
+    flex:1,
+    resizeMode: "cover",
+    
+    
+  },
+  cardimage:{
+ 
+    resizeMode:'cover',
+  },
+  Textinput1:{
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    width:350,
+    marginTop:20,
+    color:"white",
+   
+    
+  },
+  textInput: {
+  height: 40,
+  borderColor: 'black', 
+  borderWidth: 1,
+  margin: 20
+}
+});
